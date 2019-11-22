b4c4166a27b2006f7d6e97d0b914622f6155e358
JeielLovera <jeiel.disas@gmail.com>
2019-11-21 20:32:53 -0500

    aea

--- datos_insert_test.sql
+++ datos_insert_test.sql
@@ -11,5 +11,5 @@ insert into tipointereses values (1,"efectiva");
 insert into tipointereses values (2,"nominal");
 insert into usuarios values(1,"usertest","usuario1","123456","usergmail","72767516","72767516123",2);
 insert into facturas values(1,"2019-11-21","2019-12-22",12500,"factura1","soles",1);
-insert into facturas values(1,"2019-11-21","2019-12-30",15000,"factura2","soles",1);
-insert into facturas values(1,"2019-11-21","2020-01-15",14250,"factura3","soles",1);
+insert into facturas values(2,"2019-11-21","2019-12-30",15000,"factura2","soles",1);
+insert into facturas values(3,"2019-11-21","2020-01-15",14250,"factura3","soles",1);
